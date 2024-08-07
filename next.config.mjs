055bca46f64f81823b4295e227b57d66b9e56d3b
mgueyraud <mgueyraud.junior@gmail.com>
2024-08-06 22:43:31 -0400

    Fix for lanchain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        //https://github.com/langchain-ai/langchainjs/issues/5235
+        serverComponentsExternalPackages: ["langchain", "@langchain/core"],
+    },
+};
 
 export default nextConfig;
